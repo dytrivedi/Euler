4de463364faabaf8187dae578fe1fd9935a1b38f
Dhaval <trivedi.dhaval@gmail.com>
2015-07-02 11:18:34 +0530

    Better formatting

--- euler_002.js
+++ euler_002.js
@@ -12,10 +12,10 @@
 
 
 // TODO: return your answer for this prompt.
-var first = 0;
-var second = 1;
-var n = 1;
-var sum = 0;
+var first  = 0,
+    second = 1,
+    n      = 1,
+    sum    = 0;
 while (n < 4000000) {
     if (n % 2 === 0) {
         sum = sum + n;
